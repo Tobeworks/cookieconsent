d328a1db931ff0b692684d8f564daae30372ab4a
Tobias Lorsbach <hallo@tobeworks.de>
2021-12-15 15:47:20 +0100

    app init

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import Cookies from '../node_modules/js-cookie/dist/js.cookie';
+
+console.log(Cookies.get('foo'));
+
+const cookieConsenti18n = {};
+const cookieConsentOptions = {
+    mainElement: "gdpr-banner",
+    accept: 'gdpr-banner-accept',
+    decline: 'gdpr-banner-decline',
+    edit: 'gdpr-banner-edit'
+};
+
+/* bindings */
+const bannerAcceptButton = document.getElementById(cookieConsentOptions.accept);
+const bannerDeclineButton = document.getElementById(cookieConsentOptions.decline);
+const bannerEditButton = document.getElementById(cookieConsentOptions.edit);
+const bannerMainElement = document.getElementById(cookieConsentOptions.mainElement);
+
+const openCookieConsent = () => {
+    bannerMainElement.classList.remove("gdpr-cookieconsent-hidden");
+    bannerMainElement.classList.add("gdpr-cookieconsent-show");
+}
+
+const closeCookieConsent = () => {
+    bannerMainElement.classList.remove("gdpr-cookieconsent-show");
+    bannerMainElement.classList.add("gdpr-cookieconsent-hidden");
+}
+
+
+bannerAcceptButton.addEventListener('click', e => {
+    e.preventDefault();
+    closeCookieConsent();
+    console.log('klicked accept ', e);
+});
+
+bannerDeclineButton.addEventListener('click', e => {
+    e.preventDefault();
+    console.log('klicked ', e);
+});
+
+bannerEditButton.addEventListener('click', e => {
+    e.preventDefault();
+    console.log('klicked ', e);
+});
